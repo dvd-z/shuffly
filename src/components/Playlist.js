ac761649468c5235d9464372ea0fc0eb7383964c
David Zhou <david.dz.zhou@gmail.com>
2019-03-07 21:19:20 -0500

    Change playlists to selectable

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import shuffleArray from '../functions/shuffleArray';
 import Tracks from './Tracks';
 import Spotify from 'spotify-web-api-js';
 
@@ -10,42 +9,60 @@ class Playlist extends Component {
     super(props);
     this.state = {
       loadable: false,
+      selected: null,
       tracks: []
     };
   }
 
-  shufflePlaylist() {
-    if (!spotifyWebApi.getAccessToken()) {
-      alert('No Spotify access token. Please log in again.');
-      return;
-    }
+  handleClick() {
+    if (this.state.selected === null) {
+      if (!spotifyWebApi.getAccessToken()) {
+        alert('No Spotify access token. Please log in again.');
+        return;
+      }
 
-    const options = {
-      fields: 'items(track(album(images(url)),artists(name),external_urls,id,name)),limit,total',
-      limit: 3
-    };
-    spotifyWebApi.getPlaylistTracks(this.props.playlist.id, options)
-      .then(res => {
-        this.setState({
-          loadable: res.total > res.limit,
-          tracks: res.items
+      const options = {
+        fields: 'items(track(album(images(url)),artists(name),external_urls,id,name)),limit,total',
+        limit: 3
+      };
+      spotifyWebApi.getPlaylistTracks(this.props.playlist.id, options)
+        .then(res => {
+          this.setState({
+            loadable: res.total > res.limit,
+            tracks: res.items
+          });
+          // const shuffledPlaylist = shuffledPlaylist(this.state.tracks);
+        })
+        .catch(err => {
+          const response = JSON.parse(err.response);
+          console.error(response.error);
         });
-        // const shuffledPlaylist = shuffledPlaylist(this.state.tracks);
-      })
-      .catch(err => {
-        const response = JSON.parse(err.response);
-        console.error(response.error);
-      });
+    }
+    this.setState({
+      selected: !this.state.selected
+    });
   }
+
+  // shufflePlaylist(length) {
+  //   for (let i = length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [playlist[i], playlist[j]] = [playlist[j], playlist[i]];
+  //   }
+  //   return playlist;
+  // }
 
   render() {
     return (
-      <div>
+      <div onClick={() => this.handleClick()}>
         <img alt={this.props.playlist.name + " album art"} src={this.props.playlist.images[0].url} height="64"></img>
         {this.props.playlist.name} - {this.props.playlist.tracks.total} songs -
         <a href={this.props.playlist.external_urls.spotify}>Hyperlink</a>
-        <button onClick={() => this.shufflePlaylist()}>Shuffle</button>
-        <Tracks loadable={this.state.loadable} tracks={this.state.tracks} />
+        {this.state.selected &&
+          <button onClick={() => this.shufflePlaylist()}>Shuffle</button>
+        }
+        {this.state.selected &&
+          <Tracks loadable={this.state.loadable} tracks={this.state.tracks} />
+        }
       </div>
     );
   }
