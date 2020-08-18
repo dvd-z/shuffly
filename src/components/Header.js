1881ad91579ab2741a044dcd3d8432fd09e2f47f
David Zhou <david.dz.zhou@gmail.com>
2020-08-17 21:39:03 -0400

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import User from './User';
+import vector from '../images/vector.png';
 import './Header.css';
 
 class Header extends Component {
   render() {
     return (
       <header className="Header">
-        <h1 className="Header-title">Shuffly</h1>
+        <div className="Logo-container">
+          <img alt="Shuffly logo" id="Vector" src={vector}></img>
+          <div className="Line"></div>
+          <p className="Header-title">Shuffly</p>
+        </div>
         <User user={this.props.user}></User>
       </header>
     );
