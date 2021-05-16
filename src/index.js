84650aa5f61ddabba7a2de4224f4dd5c74c67a10
David Zhou <david.dz.zhou@gmail.com>
2021-05-15 23:24:28 -0400

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-require('dotenv').config();
-
-ReactDOM.render(<App />, document.getElementById('root'));
